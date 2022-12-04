a0a6d17ffc7f43da955afc8ede4c068034980c7d
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2022-12-05 04:12:55 +0500

    Sucessfully connected SignUp Page, Email Verified Page with Django backend

--- userAuthentication/forms.py
+++ userAuthentication/forms.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 class signInForm(forms.Form):
     userEmail = forms.EmailField(max_length=100,
                                  widget=forms.EmailInput(attrs={
-                                                         'placeholder': 'Your Email', 'id' : 'userEmail'}),
+                                                         'placeholder': 'Email', 'id' : 'userEmail'}),
                                  error_messages={
                                      "required": "Please fill email field to login",
                                      "max_length":"Email should be of less than 100 characters"
@@ -22,7 +22,7 @@ class signInForm(forms.Form):
 class signUpForm(forms.ModelForm):
     userEmail = forms.EmailField(required=True,max_length=100,
                                  widget=forms.EmailInput(attrs={
-                                                         'placeholder': 'Enter your Email', "class": "form-control",id:"userEmail"}),
+                                                         'placeholder': 'Email', id:"userEmail"}),
                                  error_messages={
                                      "required": "Please fill email field to login",
                                      "max_length":"Email should be of less than 100 characters"
@@ -54,33 +54,30 @@ class signUpForm(forms.ModelForm):
 
         widgets = {
             'userPassword': forms.PasswordInput(
-                attrs={'placeholder': 'Enter your Password',
-                       "class": "form-control",id:"userPassword"}
+                attrs={'placeholder': 'Password', id:"userPassword"}
             ),
             'firstName': forms.TextInput(
-                attrs={'placeholder': 'Enter your First Name',
-                       "class": "form-control",id:"firstName"},
+                attrs={'placeholder': 'First Name', id:"firstName"},
             ),
             'lastName': forms.TextInput(
-                attrs={'placeholder': 'Enter your Last Name',
-                       "class": "form-control",id:"lastName"},
+                attrs={'placeholder': 'Last Name', id:"lastName"},
             ),
         }
     confirmPassword = forms.CharField(required=True,widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter the Confirm Password', "class": "form-control","id":"confirmPassword"}),
+        'placeholder': 'Confirm Password', "id":"confirmPassword"}),
         error_messages={
         "required": "Please specify confirm password field"
     })
     organizationName= forms.CharField(max_length=100,required=False,widget=forms.TextInput(
-        attrs={'placeholder': 'Enter your Organization Name',"class": "form-control",id:"organizationName"},),
+        attrs={'placeholder': 'Organization', id:"organizationName"},),
         error_messages={
                 "max_length":"Organization Name should be of less than 100 characters"
         }
     )
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     contactNumber = forms.CharField(required=False,validators=[phone_regex], max_length=17,
-                    widget=forms.TextInput(attrs={'placeholder': 'Enter your First Name',
-                       "class": "form-control",id:"contactNumber"},),
+                    widget=forms.TextInput(attrs={'placeholder': 'Contact', 
+                                                  id:"contactNumber"},),
                     error_messages={
                         "max_length":"Contact Number should be of less than 17 characters"
                     }
